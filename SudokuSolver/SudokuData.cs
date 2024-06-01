0c60adf5d195d947cdd66078c164d2bb9d92465d
Silvero van Henningen <silvero_vanhenningen@hotmail.com>
2024-06-01 23:45:27 +0200

    deleted old folder

--- SudokuSolver/SudokuData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SudokuSolver;
-
-internal class SudokuData(int[] puzzle)
-{
-    public int[] Puzzle { get; set; } = puzzle;
-    public List<int[]> Solutions { get; set; } = [];
-    public int CalculationCycle { get; set; } = 0;
-}
