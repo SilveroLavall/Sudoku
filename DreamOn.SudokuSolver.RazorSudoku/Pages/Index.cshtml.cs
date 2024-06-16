bc15b37d18afb020979a0303dc54c069b3c8b360
Silvero van Henningen <silvero_vanhenningen@hotmail.com>
2024-06-16 20:04:15 +0200

    bootstrap

--- DreamOn.SudokuSolver.RazorSudoku/Pages/Index.cshtml.cs
+++ DreamOn.SudokuSolver.RazorSudoku/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ public class SudokuModel(ILogger<SudokuModel> logger) : PageModel
 {
     public class PageModelValues
     {
+        public string Input { get; set; } = string.Empty;
         public string SudokuPuzzle { get; set; } = string.Empty;
         public string SudokuSolution { get; set; } = string.Empty;
     }
@@ -24,10 +25,11 @@ public class SudokuModel(ILogger<SudokuModel> logger) : PageModel
     {
         logger.LogInformation("OnPost");
         logger.LogInformation("{SudokuString}", PageValues.SudokuPuzzle);
-        PageData.SudokuResponse = SudokuEngine.SolveSudoku(PageValues.SudokuPuzzle);
+        PageData.SudokuResponse = SudokuEngine.SolveSudoku(PageValues.Input);
+        PageValues.Input = PageData.SudokuResponse.SudokuRequest.Input;
         PageValues.SudokuPuzzle = PageData.SudokuResponse.SudokuRequest.Puzzle.ConvertToString();
         PageValues.SudokuSolution = PageData.SudokuResponse.Solutions.Count > 0
-            ? PageData.SudokuResponse.Solutions.First().ConvertToString()
+            ? PageData.SudokuResponse.Solutions[0].ConvertToString()
             : string.Empty;
         return Page();
     }
